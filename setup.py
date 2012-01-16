7287dbaaae9f3fe66e8b4bfcaa147f527285744c
Gawen <g@wenarab.com>
2012-01-16 13:54:40 +0100

    add setuptools script.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+
+except:
+    from distutils.core import setup
+
+import muirc
+
+
+setup(
+    name = "muirc",
+    description = "Micro IRC client",
+    
+    py_modules = ["muirc"],
+    test_suite = "tests",
+
+    version = muirc.__version__,
+    author = muirc.__author__,
+    author_email = muirc.__email__,
+    url = "http://gawen.me/p/muirc",
+    license = muirc.__license__,
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Chat",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
